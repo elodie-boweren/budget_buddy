5ab2d0f16e58d4d77f9593262f48916c01032243
Arnaud Sénégas <arnaud.senegas@laplateforme.io>
2025-03-18 10:37:44 +0100

    add class Transaction

--- budget_buddy.py
+++ budget_buddy.py
@@ -20,7 +20,6 @@ CREATE TABLE IF NOT EXISTS user (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     first_name VARCHAR(100) NOT NULL,
-    username VARCHAR(100) UNIQUE,
     email VARCHAR(100) UNIQUE NOT NULL,
     password VARCHAR(100)
 )
@@ -82,70 +81,106 @@ cursor.execute(table_transaction)
 mydb.commit()
 
 
+admin_email = ["Budgetbuddy1@laplateforme.io","Budgetbuddy2@laplateforme.io","Budgetbuddy3@laplateforme.io","Budgetbuddy4@laplateforme.io","Budgetbuddy5@laplateforme.io"]
+
 class User():
-    def __init__(self, username, passw):
-        self.username = username 
+    def __init__(self, email, passw):
+        self.email = email
         self.passw = passw
 
-    def correct_password(self, password):
+    def correct_password(self, passw):
         correct_password = re.compile(r'^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{10,}$')
-        if correct_password.match(password):
+        if correct_password.match(passw):
             return True
         return False
     
-    def enter_password(self):
-        while True:
-            self.passw =int(input("Enter your password :"))
-            if self.correct_password(self.passw):
-                cursor.execute("""
-                    INSERT INTO user (name, first_name, username, email, password)
-                    VALUES (%s, %s, %s, %s, %s)
-                    """, (self.name, self.first_name, self.username, self.email , self.passw))
-                mydb.commit()
-            else :
-                print("Invalide password, please try an other one")
-                self.enter_password()
 
-    def enter_username(self):
-        while True :
-            cursor.execute("SELECT * FROM user WHERE username = %s", (self.username,))
+    def info_check(self):
+        while True:
+            self.email = input("Enter your email :")
+            cursor.execute("SELECT * FROM user WHERE email = %s", (self.email,))
             if cursor.fetchone():
-                print("This username is already taken, please choose another one.")
-                self.enter_username()
+                print("This email is already taken, please choose another one.")
+                self.info_check()
             else :
-                break
+                self.passw =input("Enter your password :")
+                if self.correct_password(self.passw):
+                    cursor.execute("""
+                        INSERT INTO user (name, first_name, email, password)
+                        VALUES (%s, %s, %s, %s)
+                        """, (self.name, self.first_name, self.email , self.passw))
+                    mydb.commit()
+                else :
+                    print("Invalide password, please try an other one")
+                    self.info_check()
+
 
     def create_account(self):
-        self.name = int(input("Enter your name :"))
-        self.first_name =int(input("Enter your first name:"))
-        self.username = int(input("Enter your username :"))
+        self.name = input("Enter your name :")
+        self.first_name = input("Enter your first name:")
 
-        self.enter_username()
-        
-        self.email = int(input("Enter your email :"))
-
-        self.enter_password()
+        self.info_check()
 
         cursor.execute("""
-            INSERT INTO user (name, first_name, username, email, password)
+            INSERT INTO user (name, first_name, email, password)
             VALUES (%s, %s, %s, %s, %s)
-            """, (self.name, self.first_name, self.username, self.email , self.passw))
+            """, (self.name, self.first_name, self.email , self.passw))
         mydb.commit()
             
 
-    def connect_user(self):
-        cursor.execute("SELECT id, password FROM user WHERE name = %s", (self.username,))
-        result = cursor.fetchone()
-        if result : 
-            user_id = result[0]
-            passw = result[1]
-            if self.username == "admin":
+    def log_in(self):
+        self.enter_email = input("Enter your email :")
+        self.enter_password = input("Enter your password :")
+        cursor.execute("SELECT id FROM user WHERE email = %s", (self.enter_email,))
+        self.user_id = cursor.fetchall()
+        cursor.execute("SELECT password FROM user WHERE email = %s", (self.enter_email,))
+        self.user_password = cursor.fetchone()
+        cursor.execute("SELECT email FROM user")
+        self.list_email = [cursor.fetchall]
+        if self.enter_password == self.user_password and self.enter_email in self.list_email:
+            if self.email in admin_email :
                 cursor.execute("SELECT * FROM account")
-                accounts = cursor.fetchall()
+                cursor.fetchall()
+                mydb.commit()
             else :
-                cursor.execute("SELECT * FROM account WHERE user_id = %s", (user_id,))
-                accounts = cursor.fetchall()
+                cursor.execute("SELECT * FROM account WHERE user_email = %s", (self.enter_email,))
+                cursor.fetchall()
+                mydb.commit()
         else : 
             print("Wrong password, try again")
-            self.connect_user()
+            self.log_in()
 
+
+user = User()
+
+class Transaction():
+    def __init__(self):
+        self.user_id = ("SELECT id FROM user WHERE email = %s" (user.email))
+        self.user_balance = ("SELECT balance FROM amount WHERE user_id = %s", (self.user_id) )
+
+    def deposit(self, amount):
+        self.amount = amount
+        new_balance = self.user_balance + self.amount
+
+    def withdrawal(self, amount):
+        self.amount = amount
+        new_balance = self.user_balance - self.amount
+
+    def outcoming_transfert(self):
+        amount = input ("Insert amount to transfert :")
+        iban = input ("Enter IBAN : ")
+        cursor.execute("SELECT iban from account")
+        iban_list = [cursor.fetchall]
+        if iban in iban_list :
+            cursor.execute("SELECT id FROM user WHERE iban = %s" (iban))
+            self.outcoming_name = cursor.fetchall()
+            cursor.execute("SELECT balance FROM account WHERE user_id = %s" (self.outcoming_name))
+            self.outcoming_balance = cursor.fetchall()
+            cursor.execute("SELECT balance FROM account WHERE user_id = %s" (self.user_id))
+            self.balance = cursor.fetchall()
+            if self.balance > amount :
+                self.balance - amount
+                self.outcoming_balance + amount
+
+
+        
