2e05cdba885c25e9c9c49a41d4f9a52dcc0ffe94
elodie-boweren <elodie.boweren@laplateforme.io>
2025-03-20 15:24:33 +0100

    docs: new database file with type and category within transaction table (enum function)

--- database.py
+++ database.py
@@ -43,39 +43,13 @@ cursor.execute(table_account)
 mydb.commit()
 
 
-table_type = """
-CREATE TABLE IF NOT EXISTS type (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    deposit INT NOT NULL,
-    withdrawal INT NOT NULL,
-    incoming_transfer INT NOT NULL,
-    outcoming_transfer INT NOT NULL
-)
-"""
-cursor.execute(table_type)
-mydb.commit()
-
-
-table_category = """
-CREATE TABLE IF NOT EXISTS category (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100) NOT NULL
-)
-"""
-cursor.execute(table_category)
-mydb.commit()
-
-
 table_transaction = """
 CREATE TABLE IF NOT EXISTS transaction (
     id INT AUTO_INCREMENT PRIMARY KEY,
     date DATE NOT NULL,
+    type ENUM("deposit", "withdrawal", "transfer")
     description VARCHAR(100) NOT NULL,
-    reference VARCHAR(100) NOT NULL,
-    type_id INT NOT NULL,
-    category_id INT NOT NULL,
-    FOREIGN KEY (type_id) REFERENCES type(id),
-    FOREIGN KEY (category_id) REFERENCES category(id)
+    category ENUM("food", "house", "transport", "leasure", "health", "education")
 )
 """
 cursor.execute(table_transaction)
